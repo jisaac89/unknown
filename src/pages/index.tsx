64878bea354c2644b337e0dc6d262bb91d8db282
joseph isaac <josephisaac89c@gmail.com>
2019-05-10 12:08:20 -0700

    added fullpage slider version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import { Layer } from '../recoil2/components/Layer/Layer'
+import ReactFullpage from '@fullpage/react-fullpage'
 
-export default () => (
-  <Layer fill={1} scrollY>
-    <Layer flexCenter fill={1}>
-      Intro
-    </Layer>
-    <Layer flexCenter fill={1}>
-      What
-    </Layer>
-    <Layer flexCenter fill={1}>
-      Latest Work
-    </Layer>
-    <Layer flexCenter fill={1}>
-      Contact
-    </Layer>
-  </Layer>
-)
+export default () => {
+  const onSlideLeave = (origin, destination: { index: number }, direction) => {
+    console.log(destination.index)
+  }
+
+  return (
+    <ReactFullpage
+      licenseKey={'57D2108D-112842B2-B568E697-8ACF77D5'}
+      onLeave={onSlideLeave}
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <p>Section 1 (welcome to fullpage.js)</p>
+              <button onClick={() => fullpageApi.moveSectionDown()}>Click me to move down</button>
+            </div>
+            <div className="section">
+              <p>Section 2</p>
+            </div>
+          </ReactFullpage.Wrapper>
+        )
+      }}
+    />
+  )
+}
